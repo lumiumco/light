5975bc3902fdb55f9312cb84fa2946ee251136a8
Vladimir Likhanov <vladimir.likhanov@sap.com>
2025-09-21 22:59:35 +0300

    initial commit

---
+++ src/main/java/co/lumium/light/LightConfiguration.java
@@ -0,0 +1,24 @@
+package co.lumium.light;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class LightConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/actuator/health/**").permitAll()
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2-> oauth2
+                        .jwt(jwt -> jwt
+                                .jwkSetUri("http://authorization:80/oauth2/jwks")
+                        )
+                );
+        return httpSecurity.build();
+    }
+}
